5797f7cdfa999c02ad455d7c6f5b489a362d3418
Dana Palazzo <danapalazzo@Danas-MacBook.local>
2017-12-14 16:49:19 -0500

    Created wrestler model

---
+++ models/wrestlers.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const wrestlerSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  title: {type: String},
+  img: {type: String, default: 'https://thumbs.gfycat.com/FluffyIncompleteAmericanpainthorse-size_restricted.gif'}
+})
+
+
+module.exports = mongoose.model('Wrestler', wrestlerShcema);
