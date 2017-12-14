0f1467540d6a778a3ec3948e89ea239fef1d9d3f
Vernon Reis <32598819+venom9122@users.noreply.github.com>
2017-12-14 14:08:05 -0800

    routes set up for wrestler controller

--- controllers/wrestlers.js
+++ controllers/wrestlers.js
@@ -4,12 +4,14 @@ const router = express.Router();
 const Wrestler = require('../models/wrestlers');
 
 router.get('/', async (req, res) => {
-
+    const wrestlers = await Wrestler.find();
+    res.status(200).json(wrestlers);
 });
 
 router.post('/', async (req, res) => {
     try {
-
+        const wrestler = await Wrestler.create(req.body);
+        res.status(200).json(wrestler);
     } catch (err) {
         console.log(err);
         res.status(400).json({ err: err.message });
@@ -18,7 +20,8 @@ router.post('/', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
     try {
-
+        const wrestler = Wrestler.findByIdAndRemove(req.param.id);
+        res.status(200).json(book);
     } catch (err) {
         res.status(400).json({ err: err.message });
     }
@@ -26,7 +29,8 @@ router.delete('/:id', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
     try {
-
+        const wrestler = Wrestler.findByIdAndUpdate(req.param.id, req.body);
+        res.status(200).json(book);
     } catch (err) {
         console.log(err);
         res.status(400).json({ err: err.message });
