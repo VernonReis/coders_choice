b597606eff1d80af122c476ebbe3ba9bfbf6c7bd
Dana Palazzo <danapalazzo@Danas-MacBook.local>
2017-12-14 17:40:53 -0500

    Created create route

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,7 @@ const app = angular.module('Wrestlers_App', []);
 app.controller('MainController', ['$http', function ($http) {
 
     this.wrestlers = [];
+    this.newWrestlerForm = {};
 
     this.displayWrestler = function () {
         $http({
@@ -14,12 +15,23 @@ app.controller('MainController', ['$http', function ($http) {
             this.wrestlers = response.data;
         }, error => {
             console.log(error.message);
-        })
+        }).catch(err => console.log('Catch', err))
     }
 
     this.displayWrestler();
 
-
+    this.addWrestler = function () {
+      $http({
+        url: '/wrestlers',
+        method: 'POST',
+        data: {this.newWrestlerForm}
+      }).then(response => {
+        this.wrestlers.push(response.data);
+        this.newWrestlerForm = {};
+      }, error => {
+        console.log(error.message);
+      }).catch(err => console.log('Catch', err))
+    }
 
 
 }]);
