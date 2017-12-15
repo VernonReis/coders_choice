564dd3a5bc183134acd406b64d4260c8aae999e8
Vernon Reis <32598819+venom9122@users.noreply.github.com>
2017-12-14 17:09:29 -0800

    bugfixes

--- controllers/wrestlers.js
+++ controllers/wrestlers.js
@@ -29,9 +29,8 @@ router.delete('/:id', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
     try {
-        console.log(req.body);
         const wrestler = await Wrestler.findByIdAndUpdate(req.param.id, req.body);
-        //res.status(200).json(wrestler);
+        res.status(200).json(wrestler);
     } catch (err) {
         console.log(err.message);
         res.status(400).json({ err: err.message });
